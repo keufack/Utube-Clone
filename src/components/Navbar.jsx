7ce9dd32529d70ee05894a78cd3894edaaba0bd7
keufack <ngimdockkeufack@gmail.com>
2023-09-09 20:35:18 +0100

    sidebar-section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { logo } from "./utils/constants";
+import SearchBar from "./SearchBar";
+
+const Navbar = () => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#0c0c0c",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "lex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
---
+++ src/components/Video.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Video = ({ videos }) => {
+  console.log(videos);
+  return <div>f,s</div>;
+};
+
+export default Video;
