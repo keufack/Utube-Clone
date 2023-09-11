e446c9e4b122c76da898513c77f2131d62feeb98
keufack <ngimdockkeufack@gmail.com>
2023-09-11 16:26:49 +0100

    videoDetail-section

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,7 +17,7 @@ const VideoCard = ({
 }) => (
   <Card
     sx={{
-      width: { xs: "100%", sm: "358px", md: "320px" },
+      width: { xs: "100% ", sm: "358px", md: "320px" },
       boxShadow: "none",
       borderRadius: 0,
     }}
@@ -26,7 +26,7 @@ const VideoCard = ({
       <CardMedia
         image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
         alt={snippet?.title}
-        sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
+        sx={{ width: { xs: "100%", sm: "358px", md: "320px" }, height: 180 }}
       />
     </Link>
     <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
